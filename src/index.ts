c28092b80ef94161ec870aba6a0f12a5a5fd1d3c
Aleksandr Schemelev <aleksandr.schemelev>
2023-09-03 21:12:59 +0300

    chore: add division operation to eval

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,8 @@ class Eva {
       if (exp[0] === '+') return this.eval(exp[1]) + this.eval(exp[2]);
 
       if (exp[0] === '*') return this.eval(exp[1]) * this.eval(exp[2]);
+
+      if (exp[0] === '/') return this.eval(exp[1]) / this.eval(exp[2]);
     }
 
     throw 'Not implemented';
@@ -34,3 +36,4 @@ assert.strictEqual(eva.eval('"1"'), '1');
 assert.strictEqual(eva.eval(['+', 1, 2]), 3);
 assert.strictEqual(eva.eval(['+', ['+', 1, 2], 3]), 6);
 assert.strictEqual(eva.eval(['*', ['+', 1, 2], 3]), 9);
+assert.strictEqual(eva.eval(['/', ['+', 1, 2], 3]), 1);
